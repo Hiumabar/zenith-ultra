af67540c602e88166068f326b40f838cf9dc1d8a
Hiumabar <hallintar.gaming@gmail.com>
2026-02-07 20:03:43 +0700

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'zenith-ultra-v3';
+const assets = [
+  './',
+  './index.html',
+  'https://cdn.jsdelivr.net/npm/chart.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(assets))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
