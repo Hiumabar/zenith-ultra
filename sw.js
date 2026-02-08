33ffdbc794b4b892d5914e115424c0b697ae74d5
Hiumabar <hallintar.gaming@gmail.com>
2026-02-08 21:22:43 +0700

    Update service worker cache name and assets

--- sw.js
+++ sw.js
@@ -1,19 +1,17 @@
-const CACHE_NAME = 'zenith-v4';
-const assets = [
+const CACHE_NAME = 'zenith-v2';
+const ASSETS = [
   './',
   './index.html',
   './manifest.json',
-  'https://cdn.jsdelivr.net/npm/chart.js'
+  'https://i.ibb.co.com/PLCqHHz/app-icon.png',
+  'https://cdn.jsdelivr.net/npm/chart.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js'
 ];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(assets))
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
